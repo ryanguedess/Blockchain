ee975e95b9c90276d62cea41e7ba81a58c4af9ae
Ryan <ryanguedesnaoto@gmail.com>
2021-08-21 17:43:42 -0300

    Alterações no Main

--- src/com/jasi/Main.java
+++ src/com/jasi/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         //Scanner sc = new Scanner(System.in);
-        Block.generateBlockChain(20,5).print();
+        Block.generateBlockChain(100,3).print();
     }
 }
 
